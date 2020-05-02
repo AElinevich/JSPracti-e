7fb54c2383343d8c7a2dd3aa004de29a1d24b389
AElinevich <a.elinevich@tut.by>
2020-05-02 12:49:51 +0300

    create checkbox

---
+++ Checkbox/main.js
@@ -0,0 +1,10 @@
+function funcOne () {
+const checkbox = document.getElementById('one');
+console.log(one);
+
+if (checkbox.checked) {
+    console.log('Выбран');
+} else {
+    console.log('Не выбран');
+
+}};
