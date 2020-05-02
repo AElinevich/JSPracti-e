e2ac6332721514b15495635cde96d63b3f63b4ab
AElinevich <a.elinevich@tut.by>
2020-05-02 13:57:18 +0300

    create Select

--- Checkbox/main.js
+++ Checkbox/main.js
@@ -1,3 +1,4 @@
+//ChECKBOX
 function funcOne () {
 const checkbox = document.getElementById('one');
 console.log(one);
@@ -8,7 +9,7 @@ if (checkbox.checked) {
     console.log('Не выбран');
 
 }};
-
+// RADIO BUTTON
 function funcTwo() {
     const radioBtn = document.getElementsByName('r1');
     for(let i = 0; i < radioBtn.length; i++) {
@@ -17,4 +18,10 @@ function funcTwo() {
         }
     }
     
+}
+// SELECT 
+function funcThree() {
+const sel = document.getElementById('mySelect').selectedIndex;
+const options = document.getElementById('mySelect').options;
+console.log('Выбрана опция ' +options[sel].text);
 }
