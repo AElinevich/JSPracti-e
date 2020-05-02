f5a9df4aee16b33570ccd41085407f8493c80a58
AElinevich <a.elinevich@tut.by>
2020-05-02 17:07:17 +0300

    create CSS-generator

--- Checkbox/main.js
+++ Checkbox/main.js
@@ -32,3 +32,25 @@ function funcFour() {
     i1 = document.getElementById('i1');
     i1.value = range.value;
 }
+
+// CSS-generator
+function funcFive() {
+    let rtl=document.getElementById('rtl').value,
+    ttl=document.getElementById('ttl'),
+    rtr=document.getElementById('rtr').value,
+    ttr=document.getElementById('ttr'),
+    rbr=document.getElementById('rbr').value,
+    tbr=document.getElementById('tbr'),
+    rbl=document.getElementById('rbl').value,
+    tbl=document.getElementById('tbl'),
+    block=document.getElementById('block');
+
+    ttl.value=rtl;
+    ttr.value=rtr;
+    tbr.value=rbr;
+    tbl.value=rbl;
+
+    block.style.borderRadius=rtl+'px ' +rtr +'px '+rbr +'px ' +rbl+'px ';
+
+
+}
