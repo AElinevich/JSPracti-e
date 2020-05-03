21260a0dc4d9161dbe94c13a6c8267f024d693bb
AElinevich <a.elinevich@tut.by>
2020-05-03 11:19:12 +0300

    create drop-down menu

--- Checkbox/main.js
+++ Checkbox/main.js
@@ -54,3 +54,28 @@ function funcFive() {
 
 
 }
+
+// ВЫПАДАЮЩЕЕ МЕНЮ
+document.getElementById('nav').onmouseover = function(event) {
+    let target = event.target;
+    if(target.className == 'menu-item') {
+        let s = target.getElementsByClassName('submenu');
+        closeMenu();
+        s[0].style.display='block';
+
+    }
+}
+document.onmouseover = function (event) {
+    let target = event.target;
+    console.log(event.target);
+    if(target.className !='menu-item' && target.className !='submenu') {
+        closeMenu();
+    }
+}
+function closeMenu() {
+    let menu = document.getElementById('nav'),
+        subm = document.getElementsByClassName('submenu');
+        for (let i=0; i < subm.length; i++) {
+            subm[i].style.display="none";
+        }
+};
