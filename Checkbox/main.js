f34c79c89e7b92589cce35be779448170f469825
AElinevich <a.elinevich@tut.by>
2020-05-03 13:28:49 +0300

    create Animation

--- Checkbox/main.js
+++ Checkbox/main.js
@@ -132,3 +132,19 @@ window.onclick = function (event) {
     };
 }
 
+/*ANIMATION*/
+function myMove() {
+    let elem = document.getElementById("myAnimation"),
+    pos = 0,
+    id = setInterval(frame, 10);
+          function frame() {
+            if(pos == 350) {
+              clearInterval(id);
+          } else {
+              pos++;
+              elem.style.top = pos + 'px';
+              elem.style.left = pos + 'px';
+
+          }
+}
+}
