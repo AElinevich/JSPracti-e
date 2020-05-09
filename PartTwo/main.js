dc07a132673d60df728eb6e7bfeec13f92d28147
AElinevich <a.elinevich@tut.by>
2020-05-09 17:41:56 +0300

    create To Do app

--- PartTwo/main.js
+++ PartTwo/main.js
@@ -1,4 +1,5 @@
 // Progress Bar
+/*
 let btn = document.querySelector("#btn");
 btn.addEventListener('click', function move() {
     let myBar = document.querySelector("#myBar"),
@@ -29,7 +30,8 @@ for (let i=0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
 
     }
-}*/
+}
+
 
 let acc = document.querySelectorAll(".accordion"),
 panel = document.querySelectorAll(".panel");
@@ -44,4 +46,42 @@ for (let i=0; i < acc.length; i++) {
 // Money Converter
 function moneyConverter(valNum) {
     document.getElementById("outputrub").innerHTML=valNum*2.49;
+}
+*/
+// To do List
+let myNodeList = document.getElementsByTagName('LI');
+for (let i=0; i < myNodeList.length; i++) {
+    let span = document.createElement("SPAN"),
+        txt = document.createTextNode("\u00D7");
+        span.className="close";
+        span.appendChild(txt);
+        myNodeList[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+
+for (let i=0; i < close.length; i++) {
+close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+}}
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev)
+{
+    if(ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+function newElement() {
+    let li = document.createElement("li"),
+        inputValue = document.getElementById("myInputt").value,
+        t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue === ''){
+        alert('Поле не может быть пустым');
+    } else {
+        document.getElementById('myUL').appendChild(li);
+    }
+    
 }
